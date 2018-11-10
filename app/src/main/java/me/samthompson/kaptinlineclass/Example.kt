f266bc27d0a9a243ea710c61ee7377ff3ef49035
Sam Thompson <sam.thompson028@gmail.com>
2018-11-10 11:44:50 -0800

    Initial commit

---
+++ app/src/main/java/me/samthompson/kaptinlineclass/Example.kt
@@ -0,0 +1,20 @@
+package me.samthompson.kaptinlineclass
+
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+inline class LinkId(val name: String)
+
+@Entity
+data class Link(
+    @PrimaryKey val id: LinkId
+)
+
+@Database(
+    version = 1,
+    entities = [
+        Link::class
+    ]
+)
+abstract class ExampleDatabase
